9a226208d107ee30d9aea38ac346b8bd9ef5af27
Charles Lu <Charles Lu@DESKTOP-O8M4U36.resnet.wustl.edu>
2019-12-04 00:30:07 -0600

    Tears and pain

--- labs/spath/ShortestPaths.java
+++ labs/spath/ShortestPaths.java
@@ -128,8 +128,8 @@ public class ShortestPaths {
     //
     public LinkedList<Edge> returnPath(Vertex endVertex) {
     	LinkedList<Edge> path = new LinkedList<Edge>();
-    	for(Vertex to=endVertex; to!=startVertex; to=parentEdges.get(to).from){
-			path.addFirst(parentEdges.get(to));
+    	for(Vertex next=endVertex; next!=startVertex; next=parentEdges.get(next).from){
+			path.addFirst(parentEdges.get(next));
 		}
     	//
     	// FIXME: implement this using the parent edges computed in run()
